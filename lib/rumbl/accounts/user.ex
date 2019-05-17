e3615eba427f47416f0fcdc5734b205702f9c125
余 岳 <take.yo@yotake.local>
2019-05-17 17:23:36 +0900

    20190517

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -2,33 +2,27 @@ defmodule Rumbl.Accounts.User do
     use Ecto.Schema
     import Ecto.Changeset
 
-    schema "users" do
-        field :name, :string
-        field :username, :string
-
-        timestamps()
-    end
-
-    def changeset(user, attrs) do
-        user
-        |> cast(attrs, [:name, :username])
-        |> validate_required([:name, :username])
-        |> validate_length(:username, min: 1, max: 20)
-    end
 
     alias Rumbl.Accounts.Credential
 
     schema "users" do
-        field :name, :string
-        field :username, :string
-        has_one :credential, Credential
+      field :name, :string
+      field :username, :string
+      has_one :credential, Credential
 
-        timestamps()
+      timestamps()
     end
 
     def registration_changeset(user, params) do
-        user
-        |> changeset(params)
-        |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
+      user
+      |> changeset(params)
+      |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
     end
-end
+
+    def changeset(user, attrs) do
+      user
+      |> cast(attrs, [:name, :username])
+      |> validate_required([:name, :username])
+      |> validate_length(:username, min: 1, max: 20)
+    end
+  end
